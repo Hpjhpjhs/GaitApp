1347de7bb1942cb9362f40d74d0199fc76406722
Hpjhpjhs <773847441@qq.com>
2024-03-14 17:49:36 +0800

    update

---
+++ FVG/GaitApp_GaitGL_FVG/train.sh
@@ -0,0 +1,3 @@
+# # **************** For FVG ****************
+# GaitApp_GaitGL
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=23456 opengait/main_gaitapp_gaitgl_FVG.py --cfgs ./configs/gaitapp_gaitgl_FVG/gaitapp_gaitgl_FVG.yaml --phase train --log_to_file
