d407a389b468134abd1ceda5cdc71c285a75b973
Hpjhpjhs <773847441@qq.com>
2024-03-14 16:21:30 +0800

    update

---
+++ CASIA-B/GaitApp_GaitPart/test.sh
@@ -0,0 +1,4 @@
+# # **************** For CASIA-B ****************
+# GaitApp_GaitPart
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=45678 opengait/main_gaitapp_gaitpart.py --cfgs ./configs/gaitapp/gaitapp_gaitpart.yaml --phase test --log_to_file
+
